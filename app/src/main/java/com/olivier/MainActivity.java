3bf47945f49395f858c27d13470d6ad73b8643b8
b3b00 <olivier.duhart@gmail.com>
2014-03-24 19:46:51 +0100

    1.1.1 : notifications envoi et reception

--- app/src/main/java/com/olivier/MainActivity.java
+++ app/src/main/java/com/olivier/MainActivity.java
@@ -1,7 +1,11 @@
 package com.olivier;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -9,10 +13,84 @@ import android.telephony.SmsManager;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     private static final String PHONE_NO = "0660850330";
+
+    private static final String SENT      = "SMS_SENT";
+
+    private static final String DELIVERED = "SMS_DELIVERED";
+
+    protected void sendSMS(String msgKey) {
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+        String msg = pref.getString("msgDone", "");
+        String tel = pref.getString("msgRecipient", "0660850330");
+
+        android.util.Log.d("DONE", "sending SMS");
+        SmsManager smsManager = SmsManager.getDefault();
+
+
+        PendingIntent sentPI = PendingIntent.getBroadcast(this, 0, new Intent(SENT), 0);
+
+        registerReceiver(new BroadcastReceiver()
+        {
+            @Override
+            public void onReceive(Context context, Intent intent)
+            {
+                int resultCode = getResultCode();
+                // TODO
+                String message = "";
+                switch (resultCode)
+                {
+                    case Activity.RESULT_OK:
+                        message = "SMS sent";
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        message = "Generic failure";
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        message = "No service";
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        message = "Null PDU";
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        break;
+                }
+                Toast.makeText(getBaseContext(), "SMS sent", Toast.LENGTH_LONG).show();
+                TextView txt = (TextView)findViewById(R.id.txtInfo);
+                txt.setText(message);
+            }
+        }, new IntentFilter(SENT));
+
+
+        PendingIntent deliveredPI = PendingIntent.getBroadcast(this, 0, new Intent(DELIVERED), 0);
+
+        registerReceiver(new BroadcastReceiver()
+        {
+            @Override
+            public void onReceive(Context context, Intent intent)
+            {
+                // TODO
+                String pdu=intent.getStringExtra("pdu");
+                android.util.Log.i("UGO","SMS delivery intent : pdu=["+pdu+"]");
+                String message = "SMS delivered "+ (pdu != null ? "["+pdu+"]":"");
+                Toast.makeText(getBaseContext(),message,Toast.LENGTH_LONG).show();
+                TextView txt = (TextView)findViewById(R.id.txtInfo);
+                txt.setText(message);
+
+            }
+        }, new IntentFilter(DELIVERED));
+
+
+
+
+        smsManager.sendTextMessage(tel, null, msg, sentPI, deliveredPI);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,44 +102,21 @@ public class MainActivity extends Activity {
 
         buttonDone.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
-                SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
-                String msg = pref.getString("msgDone", "");
-                String tel = pref.getString("msgRecipient", "0660850330");
-
-                android.util.Log.d("DONE", "sending SMS");
-                SmsManager smsManager = SmsManager.getDefault();
-                smsManager.sendTextMessage(tel, null, msg, null, null);
+                sendSMS("msgDone");
             }
         });
 
         Button buttonOutButNotDone = (Button) findViewById(R.id.buttonOutButNotDone);
 
         buttonOutButNotDone.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
-
-                String tel = pref.getString("msgRecipient", "0660850330");
-                String msg = pref.getString("msgOutButNotDone", "");
-                android.util.Log.d("DONE", "sending SMS");
-                SmsManager smsManager = SmsManager.getDefault();
-                smsManager.sendTextMessage(tel, null, msg, null, null);
-            }
+            public void onClick(View v) {     sendSMS("msgOutButNotDone");        }
         });
 
         Button buttonNotOut = (Button) findViewById(R.id.buttonNotOut);
 
         buttonNotOut.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
-                SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
-
-                String tel = pref.getString("msgRecipient", "0660850330");
-                String msg = pref.getString("msgNotOut", "");
-                android.util.Log.d("DONE", "sending SMS");
-                SmsManager smsManager = SmsManager.getDefault();
-                smsManager.sendTextMessage(tel, null, msg, null, null);
+                sendSMS("msgNotOut");
             }
         });
 
@@ -83,9 +138,7 @@ public class MainActivity extends Activity {
     }
 
 
-    protected void sendDone(){
 
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
