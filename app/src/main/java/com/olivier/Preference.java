5b44a87068e801eb7a3103c55718227e1ff466d4
Olivier <olivier.duhart@gmail.com>
2014-03-25 18:52:32 +0100

    initial commit

---
+++ app/src/main/java/com/olivier/Preference.java
@@ -0,0 +1,15 @@
+package com.olivier;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class Preference extends PreferenceActivity {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+
+    }
+}
